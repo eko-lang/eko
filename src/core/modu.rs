3592e18b533575c2953f81610ccbd724f7502deb
Ravern Koh <ravernkoh@gmail.com>
2019-02-24 21:07:24 +0800

    Combine `structs` and `enums` into `types`

--- src/core/modu.rs
+++ src/core/modu.rs
@@ -3,7 +3,7 @@ use std::collections::BTreeMap;
 use eko_gc::{Gc, RefCell};
 
 use super::ident::Ident;
-use super::typ::{Enum, Struct};
+use super::typ::Type;
 
 #[derive(Trace)]
 pub struct Mod<'gc>(Gc<'gc, RefCell<'gc, ModData<'gc>>>);
@@ -12,6 +12,5 @@ pub struct Mod<'gc>(Gc<'gc, RefCell<'gc, ModData<'gc>>>);
 pub struct ModData<'gc> {
     parent_module: Mod<'gc>,
     child_modules: BTreeMap<Ident<'gc>, Mod<'gc>>,
-    structs: BTreeMap<Ident<'gc>, Struct<'gc>>,
-    enums: BTreeMap<Ident<'gc>, Enum<'gc>>,
+    types: BTreeMap<Ident<'gc>, Type<'gc>>,
 }
